c468de9d0ac8a4f8c0d6c6e9062ab830b4f8df76
lycyhrc <202021116012172@stu.hubu.edu.cn>
2023-07-26 22:44:19 +0800

    [new dataset] add poj_preprocess

---
+++ pykt/preprocess/poj_preprocess.py
@@ -0,0 +1,48 @@
+import pandas as pd
+
+from pykt.preprocess.utils import sta_infos,write_text
+
+KEYS =["User","Problem"]
+def read_data_from_csv(readfile, write_file):
+    stares = []
+    df = pd.read_csv(readfile)
+
+    ins, us, qs, cs, avgins, avgcq, na = sta_infos(df, KEYS, stares)
+    print(
+        f"original interaction num: {ins}, user num: {us}, question num: {qs}, concept num: {cs}, avg(ins) per s: {avgins}, avg(c) per q: {avgcq}, na: {na}")
+
+    df["index"] = range(df.shape[0])
+    df = df.dropna(subset=["User", "Problem", "Result", "Submit Time"])
+    df = df[df["Result"].isin(['Accepted', 'Wrong Answer', 'Compile Error', 'Time Limit Exceeded',
+                               'Memory Limit Exceeded', 'Runtime Error', 'Output Limit Exceeded',
+                               'Presentation Error', 'System Error', 'Validator Error'])]
+    df.loc[:, "Result"] = df.loc[:, "Result"].apply(lambda k: "1" if k == "Accepted" else "0")
+
+    ins, us, qs, cs, avgins, avgcq, na = sta_infos(df, KEYS, stares)
+    print(
+        f"after drop interaction num: {ins}, user num: {us}, question num: {qs}, concept num: {cs}, avg(ins) per s: {avgins}, avg(c) per q: {avgcq}, na: {na}")
+
+    data = []
+    ui_df = df.groupby(['User'], sort=False)
+
+    for ui in ui_df:
+        uid, curdf = ui[0], ui[1]
+        curdf.loc[:, "Submit Time"] = curdf.loc[:, "Submit Time"].apply(lambda t: change2timestamp(t, False))
+        curdf = curdf.sort_values(by=["Submit Time", "index"])
+
+        # problem -> concept
+        concepts = curdf["Problem"].astype(str)
+        responses = curdf["Result"]
+        timestamps = curdf["Submit Time"].astype(str)
+        questions = ["NA"]
+        usetimes = ["NA"]
+        uids = [str(uid), str(len(responses))]
+        data.append([uids, questions, concepts, responses, timestamps, usetimes])
+        if len(data) % 1000 == 0:
+            print(len(data))
+
+    write_text(write_file, data)
+
+    print("\n".join(stares))
+
+    return
