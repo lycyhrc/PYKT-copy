e8545091f74e613493b3cefa3124d54e7dd78b17
lycyhrc <202021116012172@stu.hubu.edu.cn>
2023-07-28 19:54:28 +0800

    add comment

--- examples/wandb_dkt_train.py
+++ examples/wandb_dkt_train.py
@@ -1,18 +1,20 @@
 import argparse
 
-from examples.wandb_train import main
+from wandb_train import main
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset_name",type=str,default="statics2011")
     parser.add_argument("--model_name", type=str, default="dkt")
-    parser.add_argument("--emb_type", type=str,default="qid")
-    parser.add_argument("--save_dir",type=str,default="save_model")
+    parser.add_argument("--emb_type", type=str,default="qid")  # 默认qid类型
+    parser.add_argument("--save_dir",type=str,default="save_model")  # 允许用户指定一个目录路径，模型和相关文件将在该目录路径中保存。
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--fold",type=int, default=0)
     parser.add_argument("--dropout", type=float, default=0.2)
+
     parser.add_argument("--emb_size",type=int, default=200)
     parser.add_argument("--learning_rate", type=float, default=1e-3)
+
     parser.add_argument("--use_wandb", type=int, default=1)
     parser.add_argument("--add_uuid", type=int, default=1)
 
